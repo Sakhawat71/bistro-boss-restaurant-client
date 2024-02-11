ce7f278e4269f40e797ded7d624e089f133ef70e
sakhawat71 <sakhawat.showrav@gmail.com>
2024-02-11 08:41:23 +0600

    navoptions

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../page/Shared/Footer/Footer";
+import Navbar from "../page/Shared/Navbar/Navbar";
 
 const Root = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <div className="max-w-7xl mx-auto">
                 <Outlet></Outlet>
             </div>
--- src/page/Shared/Navbar/Navbar.jsx
+++ src/page/Shared/Navbar/Navbar.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+
+    const navOptions = <>
+
+        <li>
+            <Link>Home</Link>
+        </li>
+
+        <li>
+            <Link>Shop</Link>
+        </li>
+
+        <li>
+            <Link>Home</Link>
+        </li>
+
+    </>
+
+
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
@@ -8,36 +28,22 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {
+                            navOptions
+                        }
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Bistro Boss</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {
+                        navOptions
+                    }
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a className="btn">Log In</a>
             </div>
         </div>
     );
