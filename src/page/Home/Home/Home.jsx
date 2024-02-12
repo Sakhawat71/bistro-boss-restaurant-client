178a96d1f789cf31e2181aeb164cb5f281d53538
sakhawat71 <sakhawat.showrav@gmail.com>
2024-02-13 02:14:12 +0600

    chefService

--- src/page/Home/Home/Home.jsx
+++ src/page/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../Banner/Banner";
+import ChefService from "../ChefService/ChefService";
 import PopularMenu from "../PopularMenu/PopularMenu";
 import Slider from "../Slider/Slider";
 
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Slider></Slider>
+            <ChefService></ChefService>
             <PopularMenu></PopularMenu>
         </div>
     );
