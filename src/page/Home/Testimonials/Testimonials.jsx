eb0312deb98ffd35681520532aa7ef270526b609
sakhawat71 <sakhawat.showrav@gmail.com>
2024-02-13 06:02:15 +0600

    add rating

--- src/page/Home/Testimonials/Testimonials.jsx
+++ src/page/Home/Testimonials/Testimonials.jsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/sectionTitle/SectionTitle";
 import axios from "axios";
-import ReviewCard from "./ReviewCard";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import './styles.css';
+import { Navigation } from 'swiper/modules';
+import { FaQuoteLeft } from "react-icons/fa";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css';
 
 const Testimonials = () => {
 
@@ -13,8 +20,11 @@ const Testimonials = () => {
             })
     }, [])
 
+    console.log(reviews)
+
     return (
-        <section className="max-w-5xl mx-auto">
+        <section className="max-w-5xl mx-auto my-20">
+
             <div className="mb-10">
                 <SectionTitle
                     subtitle={'What Our Clients Say'}
@@ -22,14 +32,29 @@ const Testimonials = () => {
                 ></SectionTitle>
             </div>
 
-            <div>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
                 {
-                    reviews.map(review => <ReviewCard
+                    reviews.map(review => <SwiperSlide
                         key={review._id}
-                        review={review}
-                    ></ReviewCard>)
+                        className="px-20"
+                    >
+                        <div className="space-y-5 ">
+
+                            <Rating
+                                className="mx-auto"
+                                style={{ maxWidth: 180 }}
+                                value={review.rating}
+                                readOnly
+                            />
+
+                            <FaQuoteLeft className="text-7xl mx-auto"></FaQuoteLeft>
+                            <p>{review.details}</p>
+                            <h2 className="text-yellow-600 text-3xl uppercase font-extralight ">{review.name}</h2>
+                        </div>
+
+                    </SwiperSlide>)
                 }
-            </div>
+            </Swiper>
         </section>
     );
 };
