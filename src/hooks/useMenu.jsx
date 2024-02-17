b8117b3a0b008d8fc34dcef7dba64b4ace0b1cfd
sakhawat71 <sakhawat.showrav@gmail.com>
2024-02-18 03:06:19 +0600

    use link

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -6,7 +6,7 @@ const useMenu = () => {
     const [menu ,setMenu] = useState([]);
 
     useEffect(()=>{
-        axios.get('menu.json')
+        axios.get('http://localhost:5000/api/v1/menu')
         .then(res => {
             setMenu(res.data)
         })
