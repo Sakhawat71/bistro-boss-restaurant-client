84ff75462797b3efd84f84d263facdeddbc40929
sakhawat71 <sakhawat.showrav@gmail.com>
2024-03-07 01:15:15 +0600

    social sign in complete with database

--- src/components/SocialSign/SocialSign.jsx
+++ src/components/SocialSign/SocialSign.jsx
@@ -3,29 +3,47 @@ import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
 
 const SocialSign = () => {
 
     const { googleSignIn } = useContext(AuthContext);
     const navigate = useNavigate();
     const from = location.state?.from?.pathname || '/';
+    const axiosPublic = useAxiosPublic()
 
     const hendelGoogleSign = () => {
         googleSignIn()
             .then(result => {
-                const user = result.user;
-                //console.log(user)
-                if (user) {
-                    Swal.fire({
-                        position: "top-end",
-                        icon: "success",
-                        title: "Wellcome",
-                        showConfirmButton: false,
-                        timer: 1000
-                    });
 
-                    navigate(from, { replace: true })
+
+                const user = result.user;
+                const userForDB = {
+                    email: user.email,
+                    name: user.displayName,
+                    photo: user.photoURL
                 }
+
+                axiosPublic.post("/api/v1/add-user",userForDB)
+                .then(res => {
+                    if(res.data.insertedId){
+                        console.log('new user')
+                    }else{
+                        console.log('old user')
+                    }
+                    
+                })
+                .catch(error => console.log('can`t send user data to DB ', error))
+
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Wellcome",
+                    showConfirmButton: false,
+                    timer: 1000
+                });
+                navigate(from, { replace: true })
+                
             })
             .catch(error => {
                 console.log(error)
