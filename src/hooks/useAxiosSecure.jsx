7f6be690f3802456b458808b4ff2d186220b148c
sakhawat71 <sakhawat.showrav@gmail.com>
2024-03-26 14:04:01 +0600

    admin home get admin states

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -6,7 +6,7 @@ import { AuthContext } from "../providers/AuthProvider";
 
 const axiosSecure = axios.create({
     baseURL: "http://localhost:5000",
-    // withCredentials: true,
+
 })
 const useAxiosSecure = () => {
     
--- src/page/Dashboard/Admin/AdminHome/AdminHome.jsx
+++ src/page/Dashboard/Admin/AdminHome/AdminHome.jsx
@@ -1,8 +1,27 @@
 
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../../hooks/useAuth";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+
 const AdminHome = () => {
+
+    const user = useAuth()
+    const axiosSecure = useAxiosSecure();
+
+    const {data : states } = useQuery({
+        queryKey: ['admin-states', user?.email],
+        // enabled : ,
+        queryFn : () => {
+            const res = axiosSecure.get('/api/v1/admin-states')
+            return res;
+        }
+    })
+
+    console.log(states?.data);
+
     return (
         <div>
-            <h2 className="text-2xl font-semibold">Hi, Welcome Back!</h2>
+            <h2 className="text-2xl font-semibold">Hi, Welcome Back! {user?.displayName ? user?.displayName : 'Admin'}</h2>
             
         </div>
     );
